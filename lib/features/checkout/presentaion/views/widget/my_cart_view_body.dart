ae903eb989717328cedea3cbfc93bba538756e06
Ahmed Alfy <ahmedalfy140@gmail.com>
2026-09-25 14:01:39 +0200

    install  flutter stripe package

--- lib/features/checkout/presentaion/views/widget/my_cart_view_body.dart
+++ lib/features/checkout/presentaion/views/widget/my_cart_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:payment_app/core/utils/app_router.dart';
+
 
 import 'package:payment_app/features/checkout/presentaion/views/widget/custom_button.dart';
 import 'package:payment_app/features/checkout/presentaion/views/widget/my_cart_total_price_widget.dart';
