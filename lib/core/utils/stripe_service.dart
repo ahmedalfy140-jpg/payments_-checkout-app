f970eff6ddd3b7cafcf1aada1466e8806ba0fcf3
Ahmed Alfy <ahmedalfy140@gmail.com>
2026-09-26 18:54:54 +0200

    create stripe service (create paymentintent method)

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,3 +1,25 @@
+
+import 'package:payment_app/core/utils/api_keys.dart';
+import 'package:payment_app/core/utils/api_service.dart';
+import 'package:payment_app/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:payment_app/features/checkout/data/models/payment_intent_model.dart';
+
 class StripeService {
-  
+  final ApiService apiService = ApiService();
+
+  Future<PaymentIntentModel> createPaymentIntent(
+    PaymentIntentInputModel paymentIntentInput,
+  ) async {
+    final response = await apiService.post(
+      body: paymentIntentInput.toJson(),
+      url: 'https://api.stripe.com/v1/payment_intents',
+      token: ApiKeys.secretKey,
+    );
+
+    final paymentIntent =
+        PaymentIntentModel.fromJson(response.data);
+
+    return paymentIntent;
+  }
 }
+
